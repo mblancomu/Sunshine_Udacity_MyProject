5200231709e12ff45184d14f8daba4f69f1ba2e3
Manuel Blanco <mblancomu@gmail.com>
2015-09-25 00:49:03 +0200

    upload proyect

---
+++ app/src/main/java/com/manuelblanco/sunshine/WeatherDataParser.java
@@ -0,0 +1,19 @@
+package com.manuelblanco.sunshine;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WeatherDataParser {
+
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+    throws JSONException{
+
+        JSONObject weather = new JSONObject(weatherJsonStr);
+        JSONArray days = weather.getJSONArray("list");
+        JSONObject dayInfo = days.getJSONObject(dayIndex);
+        JSONObject temperatureInfo = dayInfo.getJSONObject("temp");
+        return temperatureInfo.getDouble("max");
+
+    }
+}
